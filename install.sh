1d2ead34fbecca8d25b7c99e6a7b849f55ba7877
Connor <brisk.luck6031@fastmail.com>
2025-04-22 15:40:00 -0400

    Fixes and icon

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/connor5043/Plin/refs/heads/main/plin -o ~/.local/bin/plin
 chmod +x ~/.local/bin/plin
+curl -s https://raw.githubusercontent.com/connor5043/Plin/refs/heads/main/plin.svg -o ~/.local/share/icons/plin.svg
 curl -s https://raw.githubusercontent.com/connor5043/Plin/refs/heads/main/plin.desktop -o ~/.local/share/applications/plin.desktop
 update-desktop-database ~/.local/share/applications/
