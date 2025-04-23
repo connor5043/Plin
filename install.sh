2bb8a5f84e84a122d13b2d35faa449c48eae1969
Connor <brisk.luck6031@fastmail.com>
2025-04-22 21:09:04 -0400

    never would've thought...

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/icons
+mkdir -p ~/.local/share/applications
 curl -s https://raw.githubusercontent.com/connor5043/Plin/refs/heads/main/plin -o ~/.local/bin/plin
 chmod +x ~/.local/bin/plin
 curl -s https://raw.githubusercontent.com/connor5043/Plin/refs/heads/main/plin.svg -o ~/.local/share/icons/plin.svg
